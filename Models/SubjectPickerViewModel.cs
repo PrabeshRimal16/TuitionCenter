7d3abfcfe8dcfe17a73f8eb4be9a45ab5bcb0e11
Saimonoli001 <saimonoli781@gmail.com>
2026-07-27 11:30:06 +0545

    feat(student): add class, subjects, and intake fields

---
+++ Models/SubjectPickerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace TuitionCenter.Models
+{
+    public class SubjectPickerViewModel
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; } = "";
+        public List<SubjectPickerItem> Subjects { get; set; } = new();
+    }
+
+    public class SubjectPickerItem
+    {
+        public int SubjectId { get; set; }
+        public string Name { get; set; } = "";
+        public string IconText { get; set; } = "";
+        public double Rating { get; set; }
+        public string TeacherName { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int SeatsLeft { get; set; }
+        public string? Badge { get; set; }
+        public string BadgeStyle { get; set; } = "recommended";
+        public bool IsSelected { get; set; }
+    }
+}
